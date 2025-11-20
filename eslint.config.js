5e7bf74c8d52c9785c5c94dbf77d204ced41e115
Reginna Chao <rcworkadd@gmail.com>
2025-11-21 03:59:41 +0800

    chore: migrate to ESLint 9 with Flat Config (@eslint/js, remove eslint-config-standard)

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import babelParser from '@babel/eslint-parser';
+import globals from 'globals';
+
+export default [
+    js.configs.recommended,
+    prettierConfig,
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            parser: babelParser,
+            parserOptions: {
+                requireConfigFile: false,
+                babelOptions: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es6,
+                ...globals.jest,
+            },
+        },
+        plugins: {
+            prettier,
+        },
+        rules: {
+            'prettier/prettier': [
+                'error',
+                {
+                    endOfLine: 'auto',
+                },
+            ],
+            semi: [2, 'always'],
+            indent: [2, 2],
+            'no-tabs': 0,
+            'no-console': 'off',
+        },
+    },
+];
